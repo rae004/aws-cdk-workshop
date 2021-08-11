e598d2cede2f6b631a1a924272916e80003617b2
Robert Engel <robert.engel@apollidon.com>
2021-08-11 13:48:06 -0400

    adding stage to test url endpoints

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -2,8 +2,9 @@ import * as cdk from '@aws-cdk/core';
 import * as codecommit from '@aws-cdk/aws-codecommit';
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
-import { SimpleSynthAction, CdkPipeline } from "@aws-cdk/pipelines";
+import { ShellScriptAction, SimpleSynthAction, CdkPipeline } from "@aws-cdk/pipelines";
 import { WorkshopPipelineStage } from './pipeline-stage';
+
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -42,6 +43,28 @@ export class WorkshopPipelineStack extends cdk.Stack {
         })
         const deploy = new WorkshopPipelineStage(this, 'Deploy');
         // @ts-ignore
-        pipeline.addApplicationStage(deploy);
+        const deployStage = pipeline.addApplicationStage(deploy);
+        deployStage.addActions(new ShellScriptAction({
+            actionName: 'TestViewerEndpoint',
+            useOutputs: {
+                // @ts-ignore
+                ENDPOINT_URL: pipeline.stackOutput(deploy.hcViewerUrl)
+            },
+            commands: [
+                'curl -Ssf $ENDPOINT_URL'
+            ]
+        }));
+        deployStage.addActions(new ShellScriptAction({
+            actionName: 'TestAPIGatewayEndpoint',
+            useOutputs: {
+                // @ts-ignore
+                ENDPOINT_URL: pipeline.stackOutput(deploy.hcEndpoint)
+            },
+            commands: [
+                'curl -Ssf $ENDPOINT_URL/',
+                'curl -Ssf $ENDPOINT_URL/hello',
+                'curl -Ssf $ENDPOINT_URL/test'
+            ]
+        }));
     }
 }
--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,10 +1,16 @@
 import { CdkWorkshopStack } from "./cdk-workshop-stack";
-import { Stage, Construct, StageProps } from '@aws-cdk/core';
+import { Stage, Construct, StageProps, CfnOutput } from '@aws-cdk/core';
 
 export class WorkshopPipelineStage extends Stage {
+    public readonly hcViewerUrl: CfnOutput;
+    public readonly hcEndpoint: CfnOutput;
+
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
 
-        new CdkWorkshopStack(this, 'WebService')
+        const service = new CdkWorkshopStack(this, 'WebService');
+
+        this.hcEndpoint = service.hcEndpoint;
+        this.hcViewerUrl = service.hcViewerUrl;
     }
 }
