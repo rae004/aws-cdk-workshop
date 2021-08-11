513e4e2f31b0c071e217a320cd11426a0f79a578
Robert Engel <robert.engel@apollidon.com>
2021-08-11 11:47:01 -0400

    implementing our code pipeline to build our production app.

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,12 +1,43 @@
 import * as cdk from '@aws-cdk/core';
 import * as codecommit from '@aws-cdk/aws-codecommit';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import { SimpleSynthAction, CdkPipeline } from "@aws-cdk/pipelines";
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-        new codecommit.Repository(this, 'WorkshopRepo', {
+
+        const repo = new codecommit.Repository(this, 'WorkshopRepo', {
             repositoryName: 'WorkshopRepo'
         });
-        // add that pipeline code YO!
+
+        // defines the artifact that represents our sourcecode.
+        const sourceArtifact = new codepipeline.Artifact();
+
+        // defines the artifact representing of the cloud assembly
+        // (cloudformation template + all other assets)
+        const  cloudAssemblyArtifact = new codepipeline.Artifact();
+
+        // Our basic pipeline declaration. Setting the basic Structure of our pipeline.
+        const pipeline = new CdkPipeline(this, 'Pipeline', {
+            pipelineName: 'WorkshopPipeline',
+            cloudAssemblyArtifact,
+
+            // generate source artifact from the repo created above
+            sourceAction: new codepipeline_actions.CodeCommitSourceAction({
+                actionName: 'CodeCommit',
+                output: sourceArtifact,
+                repository: repo
+            }),
+
+            // builds our source code from whats declared above into a cloud assembly artifact
+            synthAction: SimpleSynthAction.standardNpmSynth({
+                sourceArtifact, // where we get teh coded to build
+                cloudAssemblyArtifact, // where to place the built source
+
+                buildCommand: 'npm run build' // language specific build cmd
+            })
+        })
     }
 }
