7b78203c725314329c7dcc813b0c76b5e25c5c04
Robert Engel <robert.engel@apollidon.com>
2021-08-11 12:32:39 -0400

    Exposing api urls so we can quickly find them

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -5,6 +5,9 @@ import { HitCounter } from "./hitcounter";
 import { TableViewer } from "cdk-dynamo-table-viewer";
 
 export class CdkWorkshopStack extends cdk.Stack {
+  public readonly hcViewerUrl: cdk.CfnOutput;
+  public readonly hcEndpoint: cdk.CfnOutput;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -16,17 +19,25 @@ export class CdkWorkshopStack extends cdk.Stack {
 
     const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
       downstream: hello
-    })
+    });
 
-    new apigw.LambdaRestApi(this, 'Endpoint', {
+    const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
       // @ts-ignore
       handler: helloWithCounter.handler
     });
 
-    new TableViewer(this, 'ViewHitCounter', {
+    const tv = new TableViewer(this, 'ViewHitCounter', {
       title: 'Hello Hits',
       table: helloWithCounter.table,
       sortBy: '-hits'
-    })
+    });
+
+    this.hcEndpoint = new cdk.CfnOutput(this, 'GatewayUrl', {
+      value: gateway.url
+    });
+
+    this.hcViewerUrl = new cdk.CfnOutput(this, 'TableViewerUrl', {
+      value: tv.endpoint
+    });
   }
 }
