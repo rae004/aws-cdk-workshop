123d29b51a76dcc1cf2c6198081c56d995fdfc0e
Robert Engel <robert.engel@apollidon.com>
2021-08-11 10:11:32 -0400

    adding test for lambda environment vars

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -17,3 +17,28 @@ test('DynamoDB Table Created', () => {
     // THEN
     expectCDK(stack).to(haveResource("AWS::DynamoDB::Table"));
 });
+
+test('Lambda Has Environment Variables', () => {
+    const stack = new cdk.Stack();
+
+    new HitCounter(stack, 'MyTestConstruct', {
+        downstream: new lambda.Function(stack, 'TestFunction', {
+            runtime: lambda.Runtime.NODEJS,
+            handler: 'lambda.handler',
+            code: lambda.Code.fromInline('test')
+        })
+    });
+
+    expectCDK(stack).to(haveResource('AWS::Lambda::Function', {
+        Environment: {
+            Variables: {
+                DOWNSTREAM_FUNCTION_NAME: {
+                    Ref: 'TestFunction22AD90FC',
+                },
+                HITS_TABLE_NAME: {
+                    Ref: 'MyTestConstructHits24A357F0',
+                }
+            }
+        }
+    }))
+})
